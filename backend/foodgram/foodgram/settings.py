5b4b1d2f05a4ed0000a20492545fd78056047d68
LinaArtmv <angelina.kislina@gmail.com>
2023-09-11 17:41:27 +0300

    Debug=False

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -4,12 +4,13 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'default')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['linaartfoodgram.sytes.net', '158.160.30.28',
                  'localhost', '127.0.0.1']
